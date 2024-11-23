8fa8a4f1d9b39e1090ed25d226a2870fd21ff2df
mattys1 <mattys0082@gmail.com>
2024-11-23 17:28:00 +0100

    mergeSort implementation

--- src/MergeSorter.cpp
+++ src/MergeSorter.cpp
@@ -5,6 +5,16 @@ template<typename T>
 class MergeSorter {
 private: 
 	std::vector<T> mergeSort(std::vector<T>& toMerge) {
+		if(toMerge.size() <= 1) {
+			return toMerge;
+		}
+		auto& [left, right] =  std::pair {
+			toMerge | std::views::take(toMerge.size() / 2),
+			toMerge | std::views::drop(toMerge.size() / 2)
+		};
+
+		auto sortedLeft = mergeSort(left);
+		auto sortedRight = mergeSort(right);
 	}
 	void merge(std::vector<T>& left, std::vector<T>& right) {
 		}
