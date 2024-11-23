176bd2b5d961602c1498f76d0169528e838bc323
mattys1 <mattys0082@gmail.com>
2024-11-23 17:24:53 +0100

    class setup

---
+++ src/MergeSorter.cpp
@@ -0,0 +1,16 @@
+#include <ranges>
+#include <vector>
+
+template<typename T>
+class MergeSorter {
+private: 
+	std::vector<T> mergeSort(std::vector<T>& toMerge) {
+	}
+	void merge(std::vector<T>& left, std::vector<T>& right) {
+		}
+	}
+public:
+	void operator() (std::vector<T>& toSort) {
+		
+	}
+};
