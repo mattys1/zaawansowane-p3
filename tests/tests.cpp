23c387a0ada7047a005fa1d9b8b8840fd15d5577
mattys1 <mattys0082@gmail.com>
2024-11-28 19:22:01 +0100

    fix equality in StabilityTest

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,3 +8,46 @@ TEST(ImportantTests, MergeSorter) {
 
 	EXPECT_EQ(test, (std::vector { 1,2,3,4,5 }));
 }
+
+TEST(ImportantTests, SortStability) {
+	struct StabilityTest {
+		int id;
+		std::string name; 
+
+		StabilityTest(int _id, std::string_view _name):
+			id { _id },
+			name { _name } {}
+
+		std::strong_ordering operator<=>(const StabilityTest& other) const {
+			return this->id <=> other.id;
+		} 
+
+		bool operator==(const StabilityTest& other) const {
+			return this->id == other.id && this->name == other.name;
+		};
+	};
+
+	std::vector stabTest { 
+		StabilityTest(1, "Polska,"),
+		StabilityTest(2, "kraj"),
+		StabilityTest(2, "piekny,"),
+		StabilityTest(3, "ale"),
+		StabilityTest(4, "ludzie"),
+		StabilityTest(5, "to"),
+	};
+
+	MergeSorter<StabilityTest> sorter;
+
+	auto sortedStabTest = stabTest;
+	sorter(sortedStabTest);
+
+	EXPECT_EQ(stabTest, sortedStabTest);
+}
+
+TEST(ImportantTests, SortReverse) {
+	MergeSorter<int> sorter;	
+	std::vector test { 5,4,3,2,1 };
+	sorter(test);
+
+	EXPECT_EQ(test, (std::vector { 1,2,3,4,5 }));
+}
