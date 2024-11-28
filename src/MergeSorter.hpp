f177123aa62917e2e585d3942e85dfa4dde2391d
mattys1 <mattys0082@gmail.com>
2024-11-28 18:07:37 +0100

    rename .cpp to .hpp; fix the crashing

---
+++ src/MergeSorter.hpp
@@ -0,0 +1,50 @@
+#include <ranges>
+#include <vector>
+
+template<typename T>
+class MergeSorter {
+private: 
+	std::vector<T> mergeSort(const std::vector<T>& toMerge) {
+		if(toMerge.size() <= 1) {
+			return toMerge;
+		}
+
+		auto left = toMerge | std::views::take(toMerge.size() / 2) | std::ranges::to<std::vector>();
+		auto right = toMerge | std::views::drop(toMerge.size() / 2) | std::ranges::to<std::vector>();
+
+		/* auto& [left, right] =  std::pair { */
+		/* 	toMerge | std::views::take(toMerge.size() / 2), */
+		/* 	toMerge | std::views::drop(toMerge.size() / 2) */
+		/* }; */
+
+		auto sortedLeft = mergeSort(left);
+		auto sortedRight = mergeSort(right);
+
+		return merge(sortedLeft, sortedRight);
+	}
+
+	std::vector<T> merge(const std::vector<T>& left, const std::vector<T>& right) {
+		std::vector<T> merged;
+		auto leftIt { left.begin() }, rightIt { right.begin() };
+
+		for (; leftIt != left.end() && rightIt != right.end();) {
+			if(*leftIt < *rightIt) {
+				merged.push_back(*leftIt);
+				leftIt++;
+			} else {
+				merged.push_back(*rightIt);
+				rightIt++;
+			}
+		}
+
+		merged.insert(merged.end(), leftIt, left.end());
+		merged.insert(merged.end(), rightIt, right.end());
+		
+		return merged;
+	}
+
+public:
+	void operator()(const std::vector<T>& toSort) {
+		mergeSort(toSort);	
+	}
+};
