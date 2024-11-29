5570d25a378acdd6861f2714efb441ca1bb84a49
mattys1 <mattys0082@gmail.com>
2024-11-29 11:40:48 +0100

    duplicate sort

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -110,3 +110,11 @@ TEST(ImportantTests, OneElemStability) {
 
 	EXPECT_EQ(beforeSortAddress, &elem[0]);
 }
+
+TEST(ImportantTests, DuplicateSort) {
+  MergeSorter<int> sorter;
+  std::vector test{5,5, 2,2, 4,4, 3,3, 1, 1};
+  sorter(test);
+
+  EXPECT_EQ(test, (std::vector{1,1, 2,2, 3,3, 4,4, 5,5}));
+}
